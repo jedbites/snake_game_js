8563dcf19a47ac179b38d9f39e191c5a445dfca7
jedbites <canon.jayde03@gmail.com>
2024-01-23 15:11:45 +0800

    Added styling to the game boards and instantiated all the variables inside js.

---
+++ script.js
@@ -0,0 +1,12 @@
+// HTML Elements
+const board = document.getElementById('game-board');
+const instructionText = document.getElementById('instruction-text');
+const logo = document.getElementById('logo');
+const score = document.getElementById('score');
+const highScoreText = document.getElementById('highScore');
+
+// Game Variables
+const gridSize = 20;
+let snake = [{x: 10, y:10}];
+let food = generateFood();
+

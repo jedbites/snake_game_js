18cb8b48e2a88c910f88874e788ef20c89899ad3
jedbites <canon.jayde03@gmail.com>
2024-01-29 18:35:11 +0800

    Modified for learning purposes.

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 // HTML Elements
 const board = document.getElementById('game-board');
-const instructionText = document.getElementById('instruction-text');
-const logo = document.getElementById('logo');
-const score = document.getElementById('score');
-const highScoreText = document.getElementById('highScore');
 
 // Game Variables
-const gridSize = 20;
-let snake = [{x: 10, y:10}];
-let food = generateFood();
+let snake = [{x:10, y:10}];
 
+// Game map, snake, and food
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+}
 
+// Draw Snake
+function drawSnake() {
+    snake.forEach((segment) => {
+        const snakeElement = createGameElement('div', 'snake');
+        setPosition(snakeElement, segment);
+        board.appendChild(snakeElement);
+    });
+}
 
+// Create html elements for food or snake
+function createGameElement(tag, className) {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element
+}
+
+// Set position for food or snake
+function setPosition(element, position) {
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+}
+
+// Testing draw function
+draw();
